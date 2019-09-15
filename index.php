cc7c8cafcb4548d5f6be4e76604b0d1223301fe9
yan menezes <yanmenezes83@gmail.com>
2019-09-15 15:35:46 -0300

    Criando o formulário

--- index.php
+++ index.php
@@ -8,23 +8,25 @@ session_start();
 <html lang="pt-br">
     <head>
         <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <title>upload</title>
         <link rel="stylesheet" type="text/css" href="<?php echo CSS_BTS . "bootstrap.min.css";?>">
     </head>
     <body>
-        <h1>Cadastrar Imagem</h1>
             <?php
                 if(isset($_SESSION['msg'])){
                     echo $_SESSION['msg'];
                     unset($_SESSION['msg']);
                 }
             ?>
-        <div class="container-fluid">  
+        <div class="container">  
+        <h1>Cadastrar Imagem</h1>
             <form method="POST" action="<?php echo BASE_URL . 'config/processa.php'?>" enctype="multipart/form-data" class="form form-horizontal">
-                <label>Imagem</label>
-                <input type="file" name="imagem"><br><br>
-                
-                <input name="enviar" type="submit" value="Cadastrar">
+                <div class="form-group">
+                    <label for="enviar">Imagem</label>
+                    <input type="file" name="imagem" class="form-control-file form-control-sm">
+                </div>
+                <button type="submit" class="btn btn-outline-primary mb-2 btn-sm">Enviar Imagem</button>
             </form>
         </div>  
         <a href="mostra.php">mostrar as fotos</a>
